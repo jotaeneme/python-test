a89bde46d39d376f045541f17f10c8839a0af76f
Juan Jose Nuno <juanjo1410@outlook.es>
2020-01-15 22:05:17 +0100

    Third

--- Chapter2/prueba3.py
+++ Chapter2/prueba3.py
@@ -10,3 +10,7 @@ else:
 #####################################################################
 
 #  Hay que prestar atención en el if: Tenemos dos escenarios donde está el and, esto nos indica que el número introducido debe ser true y true si queremos que ejecute la primera, si no en el elif se cumplirá también la primera y solo ejecutará esta
+
+
+
+#####
