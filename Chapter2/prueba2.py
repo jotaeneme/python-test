caf8b66d28696b5513a5f1149905faced72fa3bb
Juan Jose Nuno <juanjo1410@outlook.es>
2020-01-16 17:21:53 +0100

    New

--- Chapter2/prueba2.py
+++ Chapter2/prueba2.py
@@ -3,6 +3,3 @@ if edad < 18:
         print("Eres menor de edad")
 else:
         print("Eres mayor de edad")
-
-
-#HSiohfewifhb
--- Chapter2/prueba3.py
+++ Chapter2/prueba3.py
@@ -10,7 +10,3 @@ else:
 #####################################################################
 
 #  Hay que prestar atención en el if: Tenemos dos escenarios donde está el and, esto nos indica que el número introducido debe ser true y true si queremos que ejecute la primera, si no en el elif se cumplirá también la primera y solo ejecutará esta
-
-
-
-#####
